ac0a2d1ea10d84e10baa3f6895568433160c49e6
nallion <97355641+nallion@users.noreply.github.com>
2024-06-20 09:53:49 +0200

    Lastest working project from production

--- addradio.php
+++ addradio.php
@@ -1,9 +1,26 @@
+<title>Adding radio url to Iradio</title>
+<h2>Adding radio url to Iradio</h2><br>
+<form action=addradio.php method="GET">
+Radio Name: <input type="text" name="n">
+Stream URL http://<input type="text" name="u"><br><br>
+<input type="Submit" value="Add radio">
+</form>
+
 <?php
 $radioname=$_GET["n"];
 $radiourl=$_GET["u"];
+$radiourl = str_replace("http://", "", $radiourl);
+if (empty($radioname)) {
+echo "Please, enter radio name and stream url";
+die();
+}
+if (empty($radiourl)) {
+echo "Radiourl empty";
+die();
+}
 $br = exec("ffmpeg -i http://$radiourl -f null 2>&1 | grep bitrate | awk -F ' ' '{print $6}'");
 if (empty($br)) {
-  echo "Error adding radio. Cannot determine bitrate! URL INVALID!";
+  echo "Cannot determine bitrate! Error adding radio! Stream URL Invalid!";
   die();
 }
 else {
@@ -12,3 +29,4 @@ $converted_url = str_replace("/", "%/", $radiourl);
 $writetofile = "$radioname ($br kbps)%$converted_url%$br\n";
 file_put_contents('radios.txt', $writetofile, FILE_APPEND);
 }
+?>
